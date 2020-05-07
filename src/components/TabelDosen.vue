3c617e99aa560d7d00a31ae1433fcc791ddbe6d5
andisltn48 <61143381+andisltn48@users.noreply.github.com>
2020-05-07 13:38:51 -0400

    Create TabelDosen.vue

---
+++ src/components/TabelDosen.vue
@@ -0,0 +1,32 @@
+<template>
+    <b-table>
+        <template slot-scope="props">
+            <b-table-column field="id" label="ID" width="40" numeric>
+                {{ props.row.id }}
+            </b-table-column>
+
+            <b-table-column field="nama" label="Nama">
+                {{ props.row.nama }}
+            </b-table-column>
+
+            <b-table-column field="nip" label="NIP" numeric>
+                {{ props.row.nip }}
+            </b-table-column>
+
+            <b-table-column field="email" label="Email">
+                {{ props.row.email }}
+            </b-table-column>
+            
+        </template>
+    </b-table>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
