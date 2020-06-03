c0dcccd6c2d4ca5e92ae68fe513d54acb0c8bc3d
andisltn48 <61143381+andisltn48@users.noreply.github.com>
2020-06-03 08:32:02 -0700

    Add files via upload

---
+++ src/views/profil-prodi/computer-net.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="section">
+    <section class="hero is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column">
+              <figure>
+                <img src="../assets/img/cn.png" alt="">
+              </figure>
+            </div>
+            <div class="column">
+              <span><h2>Bidang Minat</h2></span>
+              <div class="title">Computer Network</div>
+              <div class="subtitle">Bidang minat Computer Network</div>
+              <div class="has-text-justified"><p>Computer Network adalah sebuah sistem yang menyambungkan komputer antar komputer maupun dengan perangkat yang lain (salah satu contohnya adalah printer) melalui media komunikasi sehingga data dan informasi dapat saling tersalurkan antar komputer maupun dengan perangkat. Selain itu, dikenal juga istilah bandwidth, yaitu kapasitas transmisi dari suatu jaringan, biasanya dilihat dari bit per detik.</p></div>
+            </div>
+          </div>
+          <div class="matakuliah">
+            <div>
+              <table-matkul/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </section>
+</template>
+<script>
+import TabelCn from '../components/TabelCn.vue';
+export default {
+  components:{
+    'table-matkul' : TabelCn
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "../assets/style/_global.scss";
+h2 {
+  background-color: black;
+  color: aliceblue;
+}
+img {
+  height: 70vh;
+  width: 35vw;
+}
+</style>
---
+++ src/views/profil-prodi/software-eng.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="section">
+    <section class="hero is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column">
+              <figure>
+                <img src="../assets/img/se.png" alt="">
+              </figure>
+            </div>
+            <div class="column">
+              <span><h2>Bidang Minat</h2></span>
+              <div class="title">Software Engineering</div>
+              <div class="subtitle">Bidang minat Software Engineering</div>
+              <div class="has-text-justified"><p>Software engineering adalah proses analisis kebutuhan dan desain pengguna, konstruksi dan uji aplikasi yang akan memenuhi kebutuhan-kebutuhan tersebut melalui penggunaan bahasa pemrograman. Secara garis besar, software engineering bisa diartikan sebagai penerapan prinsip-prinsip teknik pengembangan perangkat lunak.</p></div>
+            </div>
+          </div>
+          <div class="matakuliah">
+            <div>
+              <table-matkul/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </section>
+</template>
+<script>
+import TabelSe from '../components/TabelSe.vue';
+export default {
+  components:{
+    'table-matkul' : TabelSe
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "../assets/style/_global.scss";
+h2 {
+  background-color: black;
+  color: aliceblue;
+}
+img {
+  height: 70vh;
+  width: 35vw;
+}
+</style>
