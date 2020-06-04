80427283b1416e7486425b8e7c00c6375824dcff
andisltn48 <61143381+andisltn48@users.noreply.github.com>
2020-06-04 01:27:13 -0700

    Add files via upload

---
+++ src/views/berita/festival.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="section">
+    <section class="hero is-bold">
+      <div class="hero-body">
+        <div class="container">
+            <div class="judul-waktu">
+                <p class="title is-1">Dosen Informatika berpartisipasi dalam "BORN INNOVATION FESTIVAL"</p>
+                <p class="subtitle is-6">Pembicara - 11 November 2019 - 12:00 AM </p>
+            </div>
+            <div class="image">
+                <img src="../assets/img/telkomfest.jpg" alt="">
+            </div>
+            <div class="has-text-justified">
+                <p>Dosen Informatika berpartisipasi dalam "BORN INNOVATION FESTIVAL"  yang diadakan oleh telkom.</p>
+            </div>
+        </div>
+      </div>
+    </section>
+  </section>
+</template>
+<style lang="scss" scoped>
+@import "../assets/style/_global.scss";
+
+img {
+  height: 70vh;
+  width: 35vw;
+}
+</style>
